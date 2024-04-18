ece66583512d5ce061f75c1ef0a54192d5867203
Moon Quixote <73975308+danielmoon2001@users.noreply.github.com>
2024-04-18 22:09:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,44 @@
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+var currentStep = 1;
+var updateProgressBar;
 
-$(".next").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({
-        'transform': 'scale('+scale+')',
-        'position': 'absolute'
-      });
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
+function displayStep(stepNumber) {
+  if (stepNumber >= 1 && stepNumber <= 3) {
+    $(".step-" + currentStep).hide();
+    $(".step-" + stepNumber).show();
+    currentStep = stepNumber;
+    updateProgressBar();
+  }
+}
 
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
+  $(document).ready(function() {
+    $('#multi-step-form').find('.step').slice(1).hide();
+  
+    $(".next-step").click(function() {
+      if (currentStep < 3) {
+        $(".step-" + currentStep).addClass("animate__animated animate__fadeOutLeft");
+        currentStep++;
+        setTimeout(function() {
+          $(".step").removeClass("animate__animated animate__fadeOutLeft").hide();
+          $(".step-" + currentStep).show().addClass("animate__animated animate__fadeInRight");
+          updateProgressBar();
+        }, 500);
+      }
+    });
 
-$(".submit").click(function(){
-	return false;
-})
+    $(".prev-step").click(function() {
+      if (currentStep > 1) {
+        $(".step-" + currentStep).addClass("animate__animated animate__fadeOutRight");
+        currentStep--;
+        setTimeout(function() {
+          $(".step").removeClass("animate__animated animate__fadeOutRight").hide();
+          $(".step-" + currentStep).show().addClass("animate__animated animate__fadeInLeft");
+          updateProgressBar();
+        }, 500);
+      }
+    });
+
+    updateProgressBar = function() {
+      var progressPercentage = ((currentStep - 1) / 2) * 100;
+      $(".progress-bar").css("width", progressPercentage + "%");
+    }
+  });
