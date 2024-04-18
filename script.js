dc23b8abb57050ae6d417bf03ea5013915183e62
Moon Quixote <73975308+danielmoon2001@users.noreply.github.com>
2024-04-18 20:36:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 var currentStep = 1;
-var updateProgressBar;
+var answers = { step1: '', step2: '', step3: '' };
 
-function displayStep(stepNumber) {
-    if (stepNumber >= 1 && stepNumber <= 3) {
-        $(".step-" + currentStep).hide();
-        $(".step-" + stepNumber).show();
-        currentStep = stepNumber;
-        updateProgressBar();
-    }
+function selectAnswer(answer, step) {
+    answers['step' + step] = answer;
+    console.log("Selected: " + answer + " for step " + step); // Debugging
 }
 
 $(document).ready(function() {
-    $('#multi-step-form').find('.step').slice(1).hide();
-
-    $(".next-step").click(function() {
+    $('.next-step').click(function() {
         if (currentStep < 3) {
-            $(".step-" + currentStep).addClass("animate__animated animate__fadeOutLeft");
+            $('.step-' + currentStep).removeClass('active');
             currentStep++;
-            setTimeout(function() {
-                $(".step").removeClass("animate__animated animate__fadeOutLeft").hide();
-                $(".step-" + currentStep).show().addClass("animate__animated animate__fadeInRight");
-                updateProgressBar();
-            }, 500);
+            $('.step-' + currentStep).addClass('active');
+            updateProgressBar();
         }
     });
 
-    $(".prev-step").click(function() {
+    $('.prev-step').click(function() {
         if (currentStep > 1) {
-            $(".step-" + currentStep).addClass("animate__animated animate__fadeOutRight");
+            $('.step-' + currentStep).removeClass('active');
             currentStep--;
-            setTimeout(function() {
-                $(".step").removeClass("animate__animated animate__fadeOutRight").hide();
-                $(".step-" + currentStep).show().addClass("animate__animated animate__fadeInLeft");
-                updateProgressBar();
-            }, 500);
+            $('.step-' + currentStep).addClass('active');
+            updateProgressBar();
         }
     });
 
-    updateProgressBar = function() {
-        var progressPercentage = ((currentStep - 1) / 2) * 100;
-        $(".progress-bar").css("width", progressPercentage + "%");
+    function updateProgressBar() {
+        var progressPercentage = (currentStep - 1) / 2 * 100;
+        $('.progress-bar').css('width', progressPercentage + '%').attr('aria-valuenow', progressPercentage);
     }
 });
